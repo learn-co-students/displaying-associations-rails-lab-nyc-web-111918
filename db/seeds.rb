d9be9107e5a36526bded02a7315b04e974ae4830
lumrachele <rlum894@gmail.com>
2018-12-18 11:54:09 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Artists
+Artist.create(name: "Michael Jackson")
+Artist.create(name: "Regina Spektor")
+Artist.create(name: "The Beatles")
+puts "it's done"
+
+#Songs
+Song.create(title: "Thriller", artist_id:1)
+Song.create(title: "Yellow Submarine", artist_id:3)
+Song.create(title: "Fidelity", artist_id:2)
+Song.create(title: "Beat It", artist_id:1)
+Song.create(title: "ABC", artist_id:1)
+puts "done"
